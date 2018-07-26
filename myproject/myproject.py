3057e6e117e102b2bdc8a7f0efee5b7df5658b18
Anthony Barrera <anthony.j.barrera@gmail.com>
2018-07-26 18:23:25 -0400

    Added mongo support

--- myproject/myproject.py
+++ myproject/myproject.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from secrets import mongo_uri
+
 application = Flask(__name__)
+
+english_chatbot = ChatBot('Chatterbot',
+                         storage_adapter = "chatterbot.storage.MongoDatabaseAdapter",
+                         database = "heroku_zr537699",
+                         database_uri = mongo_uri)
+english_chatbot.set_trainer(ChatterBotCorpusTrainer)
+english_chatbot.train("chatterbot.corpus.english")
 
 @application.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
+@application.route("/chat")
+def chat():
+    input = request.args.get('input')
+    response = english_chatbot.get_response(input)
+    return response.text
+
 if __name__ == "__main__":
-    print("Hello world")
     application.run()
