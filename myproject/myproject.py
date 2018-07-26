133499b27f762f120f0cd4afbf834be1a971868c
Anthony Barrera <anthony.j.barrera@gmail.com>
2018-07-26 16:11:10 -0400

    Added stuff.

--- myproject/myproject.py
+++ myproject/myproject.py
@@ -6,4 +6,5 @@ def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    print("Hello world")
+    application.run()
